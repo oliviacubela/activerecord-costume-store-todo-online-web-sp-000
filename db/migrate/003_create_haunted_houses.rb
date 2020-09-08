867d4538f9f59272602d8b09fec3dbc97e32d4b3
Olivia Cubela <oliviacubela@gmail.com>
2020-09-08 20:35:00 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.text :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.text :opening_date
+      t.text :closing_date
+      t.text :description
+    end
+  end
+end
